dcc795a1721aad0d61e7258fdebd6f57e5970335
Jovi <jftorrevillas@gmail.com>
2016-09-07 20:20:40 +0800

    Update image_upload_script.php

--- image_upload_script.php
+++ image_upload_script.php
@@ -1,35 +1,34 @@
 <?php include "templates/include/header.php" ?>
  <?php
-// Access the $_FILES global variable for this specific file being uploaded
-// and create local PHP variables from the $_FILES array of information
-$fileName = $_FILES["uploaded_file"]["name"]; // The file name
-$fileTmpLoc = $_FILES["uploaded_file"]["tmp_name"]; // File in the PHP tmp folder
-$fileType = $_FILES["uploaded_file"]["type"]; // The type of file it is
-$fileSize = $_FILES["uploaded_file"]["size"]; // File size in bytes
-$fileErrorMsg = $_FILES["uploaded_file"]["error"]; // 0 for false... and 1 for true
-$fileName = preg_replace('#[^a-z.0-9]#i', '', "logo.png"); 
-$kaboom = explode(".", $fileName); // Split file name into an array using the dot
-$fileExt = end($kaboom); // Now target the last array element to get the file extension
-// $fileName = time().rand().".".$fileExt;
-// START PHP Image Upload Error Handling --------------------------------------------------
-if (!$fileTmpLoc) { // if file not chosen
+// Access the $_FILES global variable for this specific file being uploaded and create local PHP variables from the $_FILES array of information
+   $fileName = $_FILES["uploaded_file"]["name"]; 
+   $fileTmpLoc = $_FILES["uploaded_file"]["tmp_name"];
+   $fileType = $_FILES["uploaded_file"]["type"]; 
+   $fileSize = $_FILES["uploaded_file"]["size"]; 
+   $fileErrorMsg = $_FILES["uploaded_file"]["error"]; 
+   $fileName = preg_replace('#[^a-z.0-9]#i', '', "logo.png"); 
+   $kaboom = explode(".", $fileName); 
+   $fileExt = end($kaboom); 
+if (!$fileTmpLoc) { 
+ // if file not chosen
     echo "ERROR: Please browse for a file before clicking the upload button.";
     exit();
-} else if($fileSize > 5242880) { // if file size is larger than 5 Megabytes
+} else if($fileSize > 5242880) { 
+ // if file size is larger than 5 Megabytes
     echo "ERROR: Your file was larger than 5 Megabytes in size.";
-    unlink($fileTmpLoc); // Remove the uploaded file from the PHP temp folder
+    unlink($fileTmpLoc); 
     exit();
 } else if (!preg_match("/.(png)$/i", $fileName) ) {
      // This condition is only if you wish to allow uploading of specific file types    
      echo "ERROR: Your image was not .png.";
-     unlink($fileTmpLoc); // Remove the uploaded file from the PHP temp folder
+     unlink($fileTmpLoc);
      exit();
-} else if ($fileErrorMsg == 1) { // if file upload error key is equal to 1
+} else if ($fileErrorMsg == 1) { 
+ // if file upload error key is equal to 1
     echo "ERROR: An error occured while processing the file. Try again.";
     exit();
 }
-// END PHP Image Upload Error Handling ----------------------------------------------------
-// Place it into your "uploads" folder mow using the move_uploaded_file() function
+// Place it into  "uploads" folder using the move_uploaded_file() function
 $moveResult = move_uploaded_file($fileTmpLoc, "images/$fileName");
 // Check to make sure the move result is true before continuing
 if ($moveResult != true) {
